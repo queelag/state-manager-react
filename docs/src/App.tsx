bf83533eb7e2ef35fcb32a21e02e29131f6a333c
Dario Sechi <dario.sechi.rnd@gmail.com>
2022-07-18 19:40:59 +0200

    Optimize the useObserver amounts of re-renders

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -24,28 +24,29 @@ export function App() {
     store
   )
 
-  useObserver([store])
-
-  return (
-    <div style={{ alignItems: 'flex-start', display: 'flex', flexDirection: 'column' }}>
-      <h2>Primitives</h2>
-      <span>array: {JSON.stringify(store.array)}</span>
-      <span>bigint: {store.bigint.toString()}</span>
-      <span>boolean: {store.boolean.toString()}</span>
-      <span>function: {store.function.toString()}</span>
-      <span>null: {store.null}</span>
-      <span>number: {store.number}</span>
-      <span>object: {JSON.stringify(store.object)}</span>
-      <span>symbol: {store.symbol.toString()}</span>
-      <span>undefined: {store.undefined}</span>
-      <br />
-      <h2>Classes</h2>
-      <span>date: {store.date.toString()}</span>
-      <span>map: {JSON.stringify([...store.map.entries()])}</span>
-      <span>set: {JSON.stringify([...store.set.values()])}</span>
-      <br />
-      <h2>Libraries</h2>
-      <span>dayjs: {store.dayjs.toString()}</span>
-    </div>
+  return useObserver(
+    () => (
+      <div style={{ alignItems: 'flex-start', display: 'flex', flexDirection: 'column' }}>
+        <h2>Primitives</h2>
+        <span>array: {JSON.stringify(store.array)}</span>
+        <span>bigint: {store.bigint.toString()}</span>
+        <span>boolean: {store.boolean.toString()}</span>
+        <span>function: {store.function.toString()}</span>
+        <span>null: {store.null}</span>
+        <span>number: {store.number}</span>
+        <span>object: {JSON.stringify(store.object)}</span>
+        <span>symbol: {store.symbol.toString()}</span>
+        <span>undefined: {store.undefined}</span>
+        <br />
+        <h2>Classes</h2>
+        <span>date: {store.date.toString()}</span>
+        <span>map: {JSON.stringify([...store.map.entries()])}</span>
+        <span>set: {JSON.stringify([...store.set.values()])}</span>
+        <br />
+        <h2>Libraries</h2>
+        <span>dayjs: {store.dayjs.toString()}</span>
+      </div>
+    ),
+    [store]
   )
 }
--- src/hooks/use.observer.tsx
+++ src/hooks/use.observer.tsx
@@ -1,32 +1,7 @@
-import { noop } from '@queelag/core'
 import { watch, WatcherDisposer, WatcherType } from '@queelag/state-manager'
-import React, { Fragment, ReactElement, ReactNode, useEffect, useRef } from 'react'
+import React, { Fragment, ReactElement, ReactNode, useEffect, useMemo } from 'react'
 import { useDispatch } from './use.dispatch'
 
-/**
- * Automatically re-renders when any of the targets properties change.
- *
- * ```tsx
- * import React from 'react'
- * import { useObserver } from '@queelag/state-manager-react'
- * import { observe } from '@queelag/state-manager'
- *
- * const store = observe({ number: 0 })
- *
- * function App() {
- *   const onClick = () => {
- *     store.number++
- *   }
- *
- *   useObserver([store])
- *
- *   return <button onClick={onClick}>{store.number}</button>
- * }
- * ```
- *
- * @category Hook
- */
-export function useObserver(targets: object[]): void
 /**
  * Automatically re-renders when any of the targets properties change.
  *
@@ -48,21 +23,19 @@ export function useObserver(targets: object[]): void
  *
  * @category Hook
  */
-export function useObserver(fn: () => ReactNode, targets: object[]): ReactElement
-export function useObserver(...args: any): ReactElement {
+export function useObserver(fn: () => ReactNode, targets: object[]): ReactElement {
   const dispatch = useDispatch()
-  const fn = useRef<() => ReactNode>(args.length >= 2 ? args[0] : noop)
-  const targets = useRef<object[]>(args.length >= 2 ? args[1] : args[0])
+  const Component = useMemo(() => () => <Fragment>{fn()}</Fragment>, [])
 
   useEffect(() => {
     let disposers: WatcherDisposer[]
 
-    disposers = targets.current.map((v: object) => {
+    disposers = targets.map((v: object) => {
       return watch(WatcherType.DISPATCH, dispatch, v)
     })
 
     return () => disposers.forEach((v: WatcherDisposer) => v())
   }, [])
 
-  return <Fragment>{fn.current()}</Fragment>
+  return <Component />
 }
