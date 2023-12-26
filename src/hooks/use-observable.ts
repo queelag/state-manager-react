8dd80dbced6409e6f08dae55840602e58b99a62f
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-12-26 15:56:42 +0100

    Make the type strictness of useObservable more permissive

--- src/hooks/use-observable.ts
+++ src/hooks/use-observable.ts
@@ -1,15 +1,18 @@
+import { KeyOf } from '@aracna/core'
 import { useDispatch } from '@aracna/react'
 import { observe } from '@aracna/state-manager'
 import { DependencyList, useRef } from 'react'
 import { useReaction } from './use-reaction.js'
 
-export function useObservable<T extends object, K extends keyof T = keyof T>(target: T, keys: K[] = Object.keys(target) as K[], deps: DependencyList = []): T {
+export function useObservable<T extends object>(target: T, keys?: KeyOf.Shallow<T>[], deps?: DependencyList): T
+export function useObservable<T extends object>(target: T, keys: string[], deps?: DependencyList): T
+export function useObservable<T extends object>(target: T, keys: KeyOf.Shallow<T>[] = Object.keys(target) as any, deps: DependencyList = []): T {
   const dispatch = useDispatch()
   const observable = useRef(observe(target, keys))
 
   useReaction(
     () => {
-      keys.forEach((k: K) => observable.current[k])
+      keys.forEach((k: KeyOf.Shallow<T>) => observable.current[k])
       return NaN
     },
     dispatch,
