264e779e0d8cb0826ee22b113a2832bf8a368b6f
Dario Sechi <dario.sechi.rnd@gmail.com>
2024-01-31 19:12:14 +0100

    Make useObservable work with nested objects, maps and sets

--- src/hooks/use-observable.ts
+++ src/hooks/use-observable.ts
@@ -1,8 +1,8 @@
-import { KeyOf } from '@aracna/core'
+import { KeyOf, isArray, isPlainObject } from '@aracna/core'
 import { useDispatch } from '@aracna/react'
 import { observe } from '@aracna/state-manager'
 import { DependencyList, useRef } from 'react'
-import { useReaction } from './use-reaction.js'
+import { useAutorun } from './use-autorun.js'
 
 /**
  * Returns an observable object that can be used inside a component, changes to observed properties will trigger a re-render.
@@ -18,14 +18,40 @@ export function useObservable<T extends object>(target: T, keys: KeyOf.Shallow<T
   const dispatch = useDispatch()
   const observable = useRef(observe(target, keys))
 
-  useReaction(
-    () => {
-      keys.forEach((k: KeyOf.Shallow<T>) => observable.current[k])
-      return NaN
-    },
-    dispatch,
-    deps
-  )
+  const accessProperties = <T extends object>(object: T, keys: KeyOf.Shallow<T>[]): void => {
+    for (let key of keys) {
+      const value: unknown = object[key]
+
+      if (isArray(value) || isPlainObject(value)) {
+        accessProperties(value, Object.keys(value) as KeyOf.Shallow<typeof value>[])
+        continue
+      }
+
+      if (value instanceof Map) {
+        value.entries()
+
+        for (let k of value.keys()) {
+          value.get(k)
+        }
+
+        value.keys()
+        value.values()
+
+        continue
+      }
+
+      if (value instanceof Set) {
+        value.entries()
+        value.keys()
+        value.values()
+      }
+    }
+  }
+
+  useAutorun(() => {
+    accessProperties(observable.current, keys)
+    dispatch()
+  }, deps)
 
   return observable.current
 }
--- tests/hooks/use-observable.test.tsx
+++ tests/hooks/use-observable.test.tsx
@@ -1,5 +1,5 @@
 import { act, fireEvent, render, screen } from '@testing-library/react'
-import React, { ReactElement } from 'react'
+import React, { Fragment, ReactElement } from 'react'
 import { describe, expect, it } from 'vitest'
 import { useObservable } from '../../src'
 
@@ -8,25 +8,79 @@ describe('useObservable', () => {
     let Component: () => ReactElement
 
     Component = () => {
-      const store = useObservable({ number: 0 })
+      const store = useObservable({ array: [0], number: 0, map: new Map([['a', 0]]), object: { number: 0 }, set: new Set([0]) })
 
-      const onClick = () => {
+      const onClickArray = () => {
+        store.array[0]++
+      }
+
+      const onClickMap = () => {
+        store.map.set('a', (store.map.get('a') as number) + 1)
+      }
+
+      const onClickNumber = () => {
         store.number++
       }
 
+      const onClickObjectNumber = () => {
+        store.object.number++
+      }
+
+      const onClickSet = () => {
+        store.set.add(1)
+      }
+
       return (
-        <span data-testid='number' onClick={onClick}>
-          {store.number}
-        </span>
+        <Fragment>
+          <span data-testid='array' onClick={onClickArray}>
+            {store.array.join(',')}
+          </span>
+          <span data-testid='map' onClick={onClickMap}>
+            {store.map.get('a')}
+          </span>
+          <span data-testid='number' onClick={onClickNumber}>
+            {store.number}
+          </span>
+          <span data-testid='object.number' onClick={onClickObjectNumber}>
+            {store.object.number}
+          </span>
+          <span data-testid='set' onClick={onClickSet}>
+            {[...store.set.values()].join(',')}
+          </span>
+        </Fragment>
       )
     }
 
     render(<Component />)
+
+    expect(screen.getByTestId('array').innerHTML).toBe('0')
+    act(() => {
+      fireEvent.click(screen.getByTestId('array'))
+    })
+    expect(screen.getByTestId('array').innerHTML).toBe('1')
+
+    expect(screen.getByTestId('map').innerHTML).toBe('0')
+    act(() => {
+      fireEvent.click(screen.getByTestId('map'))
+    })
+    expect(screen.getByTestId('map').innerHTML).toBe('1')
 
     expect(screen.getByTestId('number').innerHTML).toBe('0')
     act(() => {
       fireEvent.click(screen.getByTestId('number'))
     })
     expect(screen.getByTestId('number').innerHTML).toBe('1')
+
+    expect(screen.getByTestId('object.number').innerHTML).toBe('0')
+    act(() => {
+      fireEvent.click(screen.getByTestId('object.number'))
+    })
+    expect(screen.getByTestId('object.number').innerHTML).toBe('1')
+
+    expect(screen.getByTestId('set').innerHTML).toBe('0')
+    act(() => {
+      fireEvent.click(screen.getByTestId('set'))
+    })
+    expect(screen.getByTestId('set').innerHTML).toBe('0,1')
   })
 })
